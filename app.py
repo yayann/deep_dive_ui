83e5da880b07ccecb1d750a56b70a403915d20b8
Yann Labour <yann@sugarsplashes.com>
2023-12-07 21:29:13 +0400

    addes images, sidebar and team

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import base64
+from io import StringIO
+import matplotlib.pyplot as plt
 import streamlit as st
+from matplotlib import numpy as np
+from streamlit.components.v1 import html
 from PIL import Image
 import requests
 
@@ -9,9 +14,75 @@ def get_prediction(image):
     response = requests.post(url, files={"file": img_bytes})
     return response.json()
 
-def main():
-    st.title("Deep Dive: Debris Detector")
+def svg_write(file_path, center=True):
+    # Retrieve saved string
+    with open(file_path, 'r') as file:
+        svg_string = file.read()
 
+
+    # Encode as base 64
+    b64 = base64.b64encode(svg_string.encode('utf-8')).decode('utf-8')
+
+    # Add some CSS on top
+    css_justify = "center" if center else "left"
+    css = '<p style="text-align:center; display: flex; justify-content: {};">'.format(css_justify)
+    html = r'{}<img src="data:image/svg+xml;base64,{}"/>'.format(
+        css, b64
+    )
+
+    # Write the HTML
+    st.write(html, unsafe_allow_html=True)
+
+def main():
+    font_url = "https://fonts.googleapis.com/css2?family=Original+Surfer&display=swap"
+    st.markdown(f'<link href="{font_url}" rel="stylesheet">', unsafe_allow_html=True)
+
+    css = """
+    <style>
+        .mainTitle {
+            font-family: 'Original Surfer', sans-serif;
+            font-size: 44px;
+            font-weight: bold;
+        }
+
+        .main:before {
+            content: ' ';
+            display: block;
+            position: absolute;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            opacity: 0.6;
+            background-image: url('https://i.imgur.com/GkwHy8l.jpg');
+            background-repeat: no-repeat;
+            background-position: 100% 50%;
+            background-size: cover;
+            }
+    </style>
+    """
+
+    # Inject custom CSS
+    st.markdown(css, unsafe_allow_html=True)
+
+    st.markdown("<span class='mainTitle'>Deep Dive: Debris Detector</span>", unsafe_allow_html=True)
+
+    # Sidebar
+    st.sidebar.image('https://i.imgur.com/fqkbsh2.png', width=200)
+    st.sidebar.markdown('<h3><a href="https://docs.google.com/presentation/d/1IhJCEaHFD1lr0hJHDOWrNU7ZuExr1K4zGb2GkyZeBXI/edit?usp=sharing">Presentation</h3>', unsafe_allow_html=True)
+    st.sidebar.markdown('<h3><a href="#">Demo</h3>', unsafe_allow_html=True)
+    st.sidebar.markdown('## Team Members')
+    st.sidebar.markdown(""" Priyanka Gunnoo
+                        <a href='https://www.linkedin.com/in/priyanka-gunnoo-2107/'><img src='https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/priyanka-gunnoo-2107/' alt='LinkedIn'></a>
+                        <a href='https://github.com/piiyankag'><img src='https://img.shields.io/badge/-GitHub-181717?style=flat-square&logo=GitHub&logoColor=white&link=https://github.com/piiyankag' alt='GitHub'></a>
+                        """, unsafe_allow_html=True)
+
+    st.sidebar.markdown("""Yann Labour
+                        <a href='https://www.linkedin.com/in/yannlabour//'><img src='https://img.shields.io/badge/-LinkedIn-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/yannlabour/' alt='LinkedIn'></a>
+                        <a href='https://github.com/yayann'><img src='https://img.shields.io/badge/-GitHub-181717?style=flat-square&logo=GitHub&logoColor=white&link=https://github.com/yayann' alt='GitHub'></a>
+                        """, unsafe_allow_html=True)
+
+    # Main content
     uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
     if uploaded_file is not None:
@@ -23,13 +94,15 @@ def main():
 
         # Check the category and display a custom message
         category = result.get("category")
+        probability = result.get("probabilities")[category]
+
         if category == "Background":
             st.write("No debris detected")
         else:
             # Use Markdown with custom styling for the category
             st.markdown(f"""
                 <style>
-                    .category-box {{
+                    .category-box, .proba-box  {{
                         border: 1px solid #aaa;
                         border-radius: 5px;
                         padding: 5px 10px;
@@ -37,7 +110,8 @@ def main():
                         display: inline-block;
                     }}
                 </style>
-                <div>Debris of the type <span class="category-box">{category}</span> were found in this image.</div>
+                <div>Debris of the type <span class="category-box">{category}</span> were found in this image with a probability of <span class="proba-box">{probability:.2%}.</span></div>
+
                 """, unsafe_allow_html=True)
 
         # add a spacer in between
